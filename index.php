52905ad1ddbfe4aa97c2b213d23413ce4765e3cc
tz01x <abdur963rahman@gmil.com>
2021-04-09 23:09:03 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+require("conn/db.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EWU-connect</title>
+</head>
+<body>
+    <div>
+        this is a home page of Ewu connect 
+    </div>
+</body>
+</html>
