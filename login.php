34062e30825adf12550b9bbd30b4e95380ef65a4
Habib-4-Tech <rudra.habib@gmail.com>
2021-04-12 16:07:01 +0600

    Fixed an issue with the Nav bar in Log in page

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ if($data[0]['password']==$password){
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">EWU-CONNECT</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -61,18 +61,13 @@ if($data[0]['password']==$password){
         <li class="nav-item">
           <a class="nav-link" href="http://localhost/ewu_connect/signup.php">Sign Up</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/ewu_connect/profile.php">Profile</a>
-        </li>
+      
         
   
       
        
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      
     </div>
   </div>
 </nav>
